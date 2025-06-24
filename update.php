05a06943ed8e2b709239ba6228f2b55b0da45475
denis-mwangi <denwebservices@gmail.com>
2025-06-24 12:55:17 +0300

    Update update.php

--- update.php
+++ update.php
@@ -1,12 +1,125 @@
-<div class="box">
+<section class="availability" id="availability">
+   <form action="" method="post">
+      <div class="flex">
+         <div class="box">
+            <p>check in <span>*</span></p>
+            <input type="date" name="check_in" class="input" required min="<?= date('Y-m-d'); ?>" id="check_in">
+         </div>
+         <div class="box">
+            <p>check out <span>*</span></p>
+            <input type="date" name="check_out" class="input" required id="check_out">
+         </div>
+         <div class="box">
+            <p>adults <span>*</span></p>
+            <select name="adults" class="input" id="adults" required onchange="suggestRooms()">
+               <option value="1">1 adult</option>
+               <option value="2">2 adults</option>
+               <option value="3">3 adults</option>
+               <option value="4">4 adults</option>
+               <option value="5">5 adults</option>
+               <option value="6">6 adults</option>
+            </select>
+         </div>
+         <div class="box">
+            <p>childs <span>*</span></p>
+            <select name="childs" class="input" id="childs" required onchange="suggestRooms()">
+               <option value="0">0 child</option>
+               <option value="1">1 child</option>
+               <option value="2">2 childs</option>
+               <option value="3">3 childs</option>
+               <option value="4">4 childs</option>
+               <option value="5">5 childs</option>
+               <option value="6">6 childs</option>
+            </select>
+         </div>
+         <div class="box">
+            <p>rooms <span>*</span></p>
+            <select name="rooms" class="input" id="rooms" required onchange="updateAvailabilityPrice()">
+               <option value="1">1 room</option>
+               <option value="2">2 rooms</option>
+               <option value="3">3 rooms</option>
+               <option value="4">4 rooms</option>
+               <option value="5">5 rooms</option>
+               <option value="6">6 rooms</option>
+            </select>
+         </div>
+         <div class="box">
             <p>total price <span>*</span></p>
             <input type="text" name="total_price" class="input" id="availability_price" readonly value="$0.00">
          </div>
+      </div>
+      <input type="submit" value="check availability" name="check" class="btn">
+   </form>
+</section>
 
-<div class="box">
+<section class="reservation" id="reservation">
+
+   <form action="" method="post">
+      <h3>make a reservation</h3>
+      <div class="flex">
+         <div class="box">
+            <p>your name <span>*</span></p>
+            <input type="text" name="name" maxlength="50" required placeholder="enter your name" class="input">
+         </div>
+         <div class="box">
+            <p>your email <span>*</span></p>
+            <input type="email" name="email" maxlength="50" required placeholder="enter your email" class="input">
+         </div>
+         <div class="box">
+            <p>your number <span>*</span></p>
+            <input type="number" name="number" maxlength="10" min="0" max="9999999999" required placeholder="enter your number" class="input">
+         </div>
+         <div class="box">
+            <p>rooms <span>*</span></p>
+            <select name="rooms" class="input" id="res_rooms" required onchange="updateAvailabilityPrice()">
+               <option value="1" selected>1 room</option>
+               <option value="2">2 rooms</option>
+               <option value="3">3 rooms</option>
+               <option value="4">4 rooms</option>
+               <option value="5">5 rooms</option>
+               <option value="6">6 rooms</option>
+            </select>
+         </div>
+         <div class="box">
+            <p>check in <span>*</span></p>
+            <input type="date" name="check_in" class="input" min="<?= date('Y-m-d'); ?>" required>
+         </div>
+         <div class="box">
+            <p>check out <span>*</span></p>
+            <input type="date" name="check_out" class="input" required>
+         </div>
+         <div class="box">
+            <p>adults <span>*</span></p>
+            <select name="adults" class="input" id="res_adults" required onchange="suggestReservationRooms()">
+               <option value="1" selected>1 adult</option>
+               <option value="2">2 adults</option>
+               <option value="3">3 adults</option>
+               <option value="4">4 adults</option>
+               <option value="5">5 adults</option>
+               <option value="6">6 adults</option>
+            </select>
+         </div>
+         <div class="box">
+            <p>childs <span>*</span></p>
+            <select name="childs" class="input" id="res_childs" required onchange="suggestReservationRooms()">
+               <option value="0" selected>0 child</option>
+               <option value="1">1 child</option>
+               <option value="2">2 childs</option>
+               <option value="3">3 childs</option>
+               <option value="4">4 childs</option>
+               <option value="5">5 childs</option>
+               <option value="6">6 childs</option>
+            </select>
+         </div>
+         <div class="box">
             <p>total price <span>*</span></p>
             <input type="text" name="total_price" class="input" id="reservation_price" readonly value="$0.00">
          </div>
+      </div>
+      <input type="submit" value="book now" name="book" class="btn">
+   </form>
+
+</section>
 
 <script>
 function calculateNights(checkIn, checkOut) {
